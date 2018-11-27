67215faf724b6ed69a1f37a8300b49fb0b7e42f2
Yazid Rahman Arif <yasid94@gmail.com>
2018-11-27 12:10:59 +0700

    Add files via upload

---
+++ Lazy_IoT_project.ino
@@ -0,0 +1,44 @@
+
+#define BLYNK_PRINT Serial
+
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+char auth[] = "YourAuthToken";
+
+void setup()
+{
+  // Debug console
+  Serial.begin(9600);
+
+  WiFi.mode(WIFI_STA);
+
+  int cnt = 0;
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    if (cnt++ >= 10) {
+      WiFi.beginSmartConfig();
+      while (1) {
+        delay(1000);
+        if (WiFi.smartConfigDone()) {
+          Serial.println();
+          Serial.println("SmartConfig: Success");
+          break;
+        }
+        Serial.print("|");
+      }
+    }
+  }
+
+  WiFi.printDiag(Serial);
+
+  Blynk.config(auth);
+}
+
+void loop()
+{
+  Blynk.run();
+}
+
